352b7ee15a4c69113572b93509134c52563997ac
BartoszOrnaf <bartosz.ornaf@gmail.com>
2020-02-01 18:10:11 +0100

    Updates

---
+++ precache-manifest.a77395e1cd1c3fb8bfd3d8ba06c512a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa3ac0a1857360ed2df897fab4b43b7",
+    "url": "/MuralMap/index.html"
+  },
+  {
+    "revision": "212bc548db4ad22aa9d0",
+    "url": "/MuralMap/static/css/main.e95682f5.chunk.css"
+  },
+  {
+    "revision": "27780e29fe396fd89fec",
+    "url": "/MuralMap/static/js/2.bfc8d4ce.chunk.js"
+  },
+  {
+    "revision": "212bc548db4ad22aa9d0",
+    "url": "/MuralMap/static/js/main.995f7193.chunk.js"
+  },
+  {
+    "revision": "c41753ef549bb881295b",
+    "url": "/MuralMap/static/js/runtime~main.5c7ebc37.js"
+  },
+  {
+    "revision": "63ba3c1b03604bb2d4cdaf8e454c17c4",
+    "url": "/MuralMap/static/media/MuralMap06.63ba3c1b.png"
+  }
+]);
